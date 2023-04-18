1f00da31584081c2d80802ccf2e12d2120d3c573
seyedhassanhosseini <seyedhassan.hoseiny@gmail.com>
2023-04-18 21:05:20 +0200

    new

--- api/index.js
+++ api/index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
+
 dotenv.config();
 
 mongoose
@@ -10,6 +18,10 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
