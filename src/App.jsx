f23b496df63d6e7fbc67472a07aaff15ac18bb87
seyedhassanhosseini <seyedhassan.hoseiny@gmail.com>
2023-04-18 16:35:22 +0200

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Home from "./pages/Home"
 import ProductLis from './pages/ProductList';
 import Product from "./pages/Product";
-
-
+import Register from "./pages/Register";
+import Cart from "./pages/Cart"
 function App() {
   return (
-    <div className="App">
-      <Product/>
-    </div>
+    <Home/>
   );
 }
 
