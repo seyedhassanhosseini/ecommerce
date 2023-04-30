ca355a90be451d461f5ee2d4ee15a5e5e109b51c
seyedhassanhosseini <seyedhassan.hoseiny@gmail.com>
2023-04-30 20:50:04 +0200

    new

--- api/mongo.js
+++ api/mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 // const MongoClient = require("mongodb").MongoClient;
 // var db;
-// const client = new MongoClient("mongodb+srv://seyedhassan:CVx5GIGWo5tjXXx7@cluster0.qbff56n.mongodb.net/shop?retryWrites=true&w=majority");
 // async function getData() {
 //   let result = await client.connect();
 //   db = result.db("users");
